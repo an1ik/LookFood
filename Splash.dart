f737f5801253f4afe6a77c424a4e51b7f3c94e26
Kirill <89924929+an1ik@users.noreply.github.com>
2021-09-01 20:48:54 +0500

    Update Splash.dart

--- Splash.dart
+++ Splash.dart
@@ -1,1 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:food/services/first_start.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => FirstStart())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              "assets/images/logo.svg",
+              height: 150,
+              width: 150,
+            )));
+  }
+}
+
 
