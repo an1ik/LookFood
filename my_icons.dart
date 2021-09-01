cefff4dd7dce912be1ebdb6e8056ba1e93e63d1e
Kirill <89924929+an1ik@users.noreply.github.com>
2021-09-01 21:25:59 +0500

    Create my_icons.dart

---
+++ my_icons.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class MyIcons {
+  MyIcons._();
+
+  static const _kFontFam = 'MyIcons';
+  static const String _kFontPkg = null;
+
+  static const IconData cross = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData applelogo = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData f = IconData(0xe805, fontFamily: _kFontFam, font-Package: _kFontPkg);
+  static const IconData time = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profile = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData leaf = IconData(0xf06c, fontFamily: _kFontFam, font-Package: _kFontPkg);
+  static const IconData google = IconData(0xf1a0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
